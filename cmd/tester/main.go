6bf97cb9bb87333c8fc245883f3d4beba25b4150
jazz <1337jazz@gmail.com>
2024-11-22 20:21:43 +0100

    Update - Fri 22 Nov 20:21:43 CET 2024

--- cmd/tester/main.go
+++ cmd/tester/main.go
@@ -10,7 +10,8 @@ import (
 )
 
 type Response struct {
-	Url string `json:"url"`
+	Url     string `json:"url"`
+	TagName string `json:"tag_name"`
 }
 
 //go:embed version.txt
@@ -32,7 +33,14 @@ func main() {
 		panic(err)
 	}
 
-	url := release.Url
+	tagName := release.TagName
+
+	if tagName == version {
+		fmt.Println("Already up to date")
+		return
+	}
+
+	url := fmt.Sprintf("https://github.com/1337jazz/go-update-test/releases/download/%s/go-update-test_Linux_x86_64.tar.gz", tagName)
 
 	err = doUpdate(url)
 	if err != nil {
