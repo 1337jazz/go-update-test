830f82548cd6770165f472e3cf8f369cf71fa86e
jazz <1337jazz@gmail.com>
2024-11-22 20:01:07 +0100

    Update - Fri 22 Nov 20:01:07 CET 2024

--- cmd/tester/main.go
+++ cmd/tester/main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/inconshreveable/go-update"
 )
 
+type Response struct {
+	Url string `json:"url"`
+}
+
 func main() {
-	url := "https://github.com/1337jazz/go-update-test/releases/download/0.0.3/go-update-test_Linux_x86_64.tar.gz"
-	err := doUpdate(url)
+
+	var latestReleaseUrl = "https://api.github.com/repos/1337jazz/go-update-test/releases/latest"
+	resp, err := http.Get(latestReleaseUrl)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Update successful, version 0.0.5 now!")
+	defer resp.Body.Close()
+
+	// Decode the JSON response into our struct type.
+	var release Response
+	err = json.NewDecoder(resp.Body).Decode(&release)
+	if err != nil {
+		panic(err)
+	}
+
+	url := release.Url
+
+	err = doUpdate(url)
+	if err != nil {
+		panic(err)
+	}
+
+	// get the version from version.txt
+
+	data, err := os.ReadFile("version.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Updated to version: ", string(data))
 }
 
 func doUpdate(url string) error {
